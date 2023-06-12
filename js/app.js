71eff44606c196dafcd13d683c2ddbdc73e3d0b9
Iván Rodríguez <ivan18rt@outlook.com>
2023-06-11 18:33:48 -0600

    pico css añadido

--- js/app.js
+++ js/app.js
@@ -2,13 +2,18 @@ const fechaSeleccionada = document.querySelector("#fechaSeleccionada");
 const formDias = document.querySelector("#formDias");
 const dias = document.querySelector("#dias");
 const newFecha = document.querySelector(".newFecha");
+const newFechaEscrita = document.querySelector(".newFechaEscrita");
 
 formDias.addEventListener("submit", (e) => {
   e.preventDefault();
   if (fechaSeleccionada.value.length > 0) {
     const nuevaFecha = moment(fechaSeleccionada.value).add(dias.value, "d");
-    console.log(nuevaFecha._i);
+    // console.log(nuevaFecha._i);
     newFecha.textContent = nuevaFecha.format("DD-MM-YYYY");
+
+    const fechaEscrita = moment(nuevaFecha._i).format("dddd, MMMM Do YYYY");
+    // console.log(fechaEscrita);
+    newFechaEscrita.textContent = fechaEscrita;
   } else {
     console.log("No se ha seleccionado una fecha");
     alert("No se ha seleccionado una fecha");
